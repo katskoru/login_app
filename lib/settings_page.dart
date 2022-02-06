c3bb35e0e33fc0f1b62198758f84d1696c7e1d0d
katskoru <katskoru@gmail.com>
2022-02-06 18:45:44 +0100

    settings_buttons

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -14,51 +14,72 @@ class _SettingsPageState extends State<SettingsPage> {
     return Scaffold(
         body: Column(
       children: [
-        ExpansionTile(
-          title: Text(
-            "General Settings",
-            style: TextStyle(fontSize: 30),
+        Theme(
+          data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+          child: ExpansionTile(
+            title: Text(
+              "General Settings",
+              style: TextStyle(fontSize: 30),
+            ),
+            trailing: Icon(
+              _customTileExpanded ? Icons.arrow_drop_up : Icons.arrow_drop_down,
+              size: 40.0,
+            ),
+            children: <Widget>[
+              ListTile(
+                  title: Text(
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam finibus felis eros, id vulputate tellus vehicula non. Ut vitae laoreet lorem. Donec ac metus vestibulum erat semper fermentum. Etiam varius lectus et convallis gravida. Aenean id porta odio, faucibus malesuada orci.")),
+            ],
           ),
-          trailing: Icon(
-            _customTileExpanded ? Icons.arrow_drop_up : Icons.arrow_drop_down,
-            size: 40.0,
-          ),
-          children: <Widget>[
-            ListTile(
-                title: Text(
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam finibus felis eros, id vulputate tellus vehicula non. Ut vitae laoreet lorem. Donec ac metus vestibulum erat semper fermentum. Etiam varius lectus et convallis gravida. Aenean id porta odio, faucibus malesuada orci.")),
-          ],
         ),
-        ExpansionTile(
-          title: const Text(
-            "User Settings",
-            style: TextStyle(fontSize: 30),
-          ),
-          trailing: Icon(
-            _customTileExpanded ? Icons.arrow_drop_up : Icons.arrow_drop_down,
-            size: 40.0,
-          ),
-          children: const <Widget>[
-            ListTile(
-                title: Text(
-              "Quisque lacus magna, interdum blandit orci at, porttitor placerat enim. Maecenas rutrum, nulla et porta efficitur, nisi diam pellentesque ex, nec commodo turpis ligula quis tellus. ",
-            )),
-          ],
-          onExpansionChanged: (bool expanded) {
-            setState(() => _customTileExpanded = expanded);
-          },
+        Divider(
+          height: 10.0,
+          color: Colors.black,
         ),
-        ExpansionTile(
-          title: Text("Data Policy", style: TextStyle(fontSize: 30)),
-          trailing: Icon(
-            _customTileExpanded ? Icons.arrow_drop_up : Icons.arrow_drop_down,
-            size: 40.0,
+        Theme(
+          data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+          child: ExpansionTile(
+            title: const Text(
+              "User Settings",
+              style: TextStyle(fontSize: 30),
+            ),
+            trailing: Icon(
+              _customTileExpanded ? Icons.arrow_drop_up : Icons.arrow_drop_down,
+              size: 40.0,
+            ),
+            children: const <Widget>[
+              ListTile(
+                  title: Text(
+                "Quisque lacus magna, interdum blandit orci at, porttitor placerat enim. Maecenas rutrum, nulla et porta efficitur, nisi diam pellentesque ex, nec commodo turpis ligula quis tellus. ",
+              )),
+            ],
+            onExpansionChanged: (bool expanded) {
+              setState(() => _customTileExpanded = expanded);
+            },
           ),
-          children: <Widget>[
-            ListTile(
-                title: Text(
-                    "Pellentesque tellus justo, elementum vitae sagittis faucibus, varius quis nunc.")),
-          ],
+        ),
+        Divider(
+          height: 10.0,
+          color: Colors.black,
+        ),
+        Theme(
+          data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+          child: ExpansionTile(
+            title: Text("Data Policy", style: TextStyle(fontSize: 30)),
+            trailing: Icon(
+              _customTileExpanded ? Icons.arrow_drop_up : Icons.arrow_drop_down,
+              size: 40.0,
+            ),
+            children: <Widget>[
+              ListTile(
+                  title: Text(
+                      "Pellentesque tellus justo, elementum vitae sagittis faucibus, varius quis nunc.")),
+            ],
+          ),
+        ),
+        Divider(
+          height: 10.0,
+          color: Colors.black,
         ),
       ],
     ));
