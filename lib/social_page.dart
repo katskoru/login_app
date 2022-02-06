95e8c6fefff1c247085addc16e527143137c72e0
katskoru <katskoru@gmail.com>
2022-02-06 13:43:36 +0100

    settings_page

--- lib/social_page.dart
+++ lib/social_page.dart
@@ -1,20 +1,119 @@
 import 'package:flutter/material.dart';
+
 import 'package:login_page/feed_page.dart';
 
-class SocialPage extends StatelessWidget {
+class SocialPage extends StatefulWidget {
   const SocialPage({Key? key}) : super(key: key);
 
   @override
+  State<SocialPage> createState() => _SocialPageState();
+}
+
+class _SocialPageState extends State<SocialPage> {
+  PageController controller = PageController();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      endDrawer: Drawer(),
+      endDrawer: myDrawer(),
       appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        backgroundColor: Colors.white,
+        elevation: 10.0,
         title: Text(
           "Social_Page",
           style: TextStyle(color: Colors.black),
         ),
       ),
-      body: FeedPage(),
+      body: PageView(
+        controller: controller,
+        physics: NeverScrollableScrollPhysics(),
+        children: [FeedPage(), Container()],
+      ),
+    );
+  }
+
+  myDrawer() {
+    return Drawer(
+      elevation: 20.0,
+      child: ListView(
+        children: [
+          SizedBox(
+            height: 40.0,
+          ),
+          SizedBox(
+            height: 75.0,
+            child: ListTile(
+              contentPadding: EdgeInsets.all(8.0),
+              leading: const Icon(
+                Icons.explore,
+                color: Colors.black,
+                size: 68.0,
+              ),
+              title: const Text(
+                "Home",
+                style: TextStyle(fontSize: 40.0),
+              ),
+              onTap: () {
+                controller.animateToPage(0,
+                    duration: Duration(milliseconds: 400),
+                    curve: Curves.easeIn);
+              },
+            ),
+          ),
+          SizedBox(
+            height: 75.0,
+            child: ListTile(
+              contentPadding: EdgeInsets.all(10.0),
+              leading: const Icon(
+                Icons.account_circle,
+                color: Colors.black,
+                size: 68.0,
+              ),
+              title: const Text(
+                "Profile",
+                style: TextStyle(fontSize: 40.0),
+              ),
+              onTap: () {},
+            ),
+          ),
+          SizedBox(
+            height: 75.0,
+            child: ListTile(
+              contentPadding: EdgeInsets.all(10.0),
+              leading: const Icon(
+                Icons.chat,
+                color: Colors.black,
+                size: 68.0,
+              ),
+              title: const Text(
+                "Chat",
+                style: TextStyle(fontSize: 40.0),
+              ),
+              onTap: () {},
+            ),
+          ),
+          SizedBox(
+            height: 75.0,
+            child: ListTile(
+              contentPadding: EdgeInsets.all(10.0),
+              leading: const Icon(
+                Icons.settings,
+                color: Colors.black,
+                size: 68.0,
+              ),
+              title: const Text(
+                "Settings",
+                style: TextStyle(fontSize: 40.0),
+              ),
+              onTap: () {
+                controller.animateToPage(1,
+                    duration: Duration(milliseconds: 400),
+                    curve: Curves.easeIn);
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
