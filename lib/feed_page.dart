bdc9f432b8c243fdb5732c8f8d9622398992d501
katskoru <katskoru@gmail.com>
2022-02-06 21:45:19 +0100

    Textbutton

--- lib/feed_page.dart
+++ lib/feed_page.dart
@@ -11,11 +11,18 @@ class _FeedPageState extends State<FeedPage> {
   Widget build(BuildContext context) {
     List<String> imgList = [
       "https://images.pexels.com/photos/7363753/pexels-photo-7363753.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    ];
+    List<String> imgList2 = [
       "https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
       "https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
       "https://images.pexels.com/photos/9748197/pexels-photo-9748197.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      "https://images.pexels.com/photos/1070945/pexels-photo-1070945.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+      "https://images.pexels.com/photos/1070945/pexels-photo-1070945.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     ];
+    int index1 = 0;
+    setState(() {
+      index1 += 1;
+    });
+
     return Stack(
       children: [
         ListView.builder(
@@ -29,7 +36,7 @@ class _FeedPageState extends State<FeedPage> {
               return MyCard(
                 myHandler: () {
                   setState(() {
-                    print("object");
+                    func;
                   });
                 },
                 imgUrl: imgList[index],
@@ -44,11 +51,16 @@ class _FeedPageState extends State<FeedPage> {
             width: 80.0,
             child: FloatingActionButton(
               backgroundColor: Colors.purple,
-              child: Icon(
+              child: const Icon(
                 Icons.add_photo_alternate,
                 size: 50.0,
               ),
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  imgList.add(
+                      "https://images.pexels.com/photos/1070945/pexels-photo-1070945.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500");
+                });
+              },
             ),
           ),
         ),
